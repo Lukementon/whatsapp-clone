4e0d3f4d1aaba5772fffe7885d76aae4b61bb85a
Luke Menton <lukementon15@gmail.com>
2021-07-17 16:58:22 +0100

    Image

--- components/Loading.js
+++ components/Loading.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Circle } from "better-react-spinkit";
+import Image from "next/image";
 
 const Loading = () => {
   return (
@@ -11,11 +12,12 @@ const Loading = () => {
       }}
     >
       <div>
-        <img
+        <Image
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
           alt=""
           style={{ marginBottom: 10 }}
           height={200}
+          width={200}
         />
         <Circle color="#3CBC28" size={60} />
       </div>
